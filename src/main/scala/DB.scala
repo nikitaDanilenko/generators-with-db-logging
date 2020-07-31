d2c19fe24fe3024bed723beb2cc4e33f3c3c1f11
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-07-31 19:51:49 +0200

    Make DB type lazy so that computations are delayed

--- src/main/scala/DB.scala
+++ src/main/scala/DB.scala
@@ -1,22 +1,31 @@
 import scala.concurrent.Future
 
-case class DB[A](value: A) {
+case class DB[A](value: () => A) {
 
   def flatMap[B](f: A => DB[B]): DB[B] =
-    f(value)
+    f(value())
 
   def map[B](f: A => B): DB[B] =
-    DB(f(value))
+    DB(() => f(value()))
 
 }
 
 object DB {
-  def successful[A](a: A): DB[A] = DB(a)
+
+  def successful[A](a: => A): DB[A] = DB(() => a)
 
   def failed[A](message: String): DB[A] = throw new Throwable(message)
 
   def sequence[A](actions: Vector[DB[A]]): DB[Vector[A]] =
-    DB(actions.map(_.value))
+    DB(() => actions.map(_.value()))
 
-  def run[A](action: DB[A]): Future[A] = Future.successful(action.value)
+  def seq(actions: Vector[DB[_]]): DB[Unit] = {
+    DB {
+      () =>
+        actions.foldLeft(())((_, action) => action.value())
+    }
+
+  }
+
+  def run[A](action: DB[A]): Future[A] = Future.successful(action.value())
 }
